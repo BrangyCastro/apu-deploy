9e8b474919faafa2624d94932b2b8c173a3253e2
Brangy Castro <brangy.castro1@gmail.com>
2022-06-12 22:40:39 -0500

    Config

--- frontend/src/domain/utils/constant.js
+++ frontend/src/domain/utils/constant.js
@@ -1,2 +1,3 @@
-export const API_KEY = process.env.REACT_APP_REST || "http://localhost:8000/v3";
+export const API_KEY =
+  process.env.REACT_APP_REST || "http://localhost:8000/api/v3";
 export const ACCESS_TOKEN = "apu_token";
